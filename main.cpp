5550bc7b8e8474b6c212e6eed83b745a0baac716
Carlos-Lopes <hlopes.ec@gmail.com>
2017-11-20 01:28:18 -0200

    -Implementado função first_fit() e next_fit() -Correção nas funções mostra_mem() e inicializa_mem()

--- main.cpp
+++ main.cpp
@@ -1,67 +1,112 @@
 #include <iostream>
 
-struct block {
-    int size;
-};
-
-struct process {
-    int size;
-};
-
 struct memory {
     struct memory *next;
-    struct block *block;
-    struct process *process;
+    int size;
 };
-unsigned int memory_size = 2048;
-struct memory *_memory;
+
+unsigned int memory_size = 1024;
+struct memory *_memory = nullptr;
+struct memory *base_memory_address = nullptr;
 
 void inicializa_mem() {
     _memory = (memory *) malloc(memory_size);
+    _memory->size = memory_size - sizeof(struct memory);
     _memory->next = nullptr;
-    _memory->block = nullptr;
-    _memory->process = nullptr;
-    std::cout << "Memory Adress:" << _memory << std::endl;
-    std::cout << "Next Block: " << _memory->block << std::endl;
-    std::cout << "Next Process: " << _memory->process << std::endl;
 }
 
 void mostra_mem() {
-    std::cout << _memory << std::endl;
     auto *iterator = _memory;
     while (iterator != nullptr) {
-        if (iterator->block != nullptr)
-            std::cout << "Block:" << iterator->block->size << std::endl;
-
-        if (iterator->process != nullptr)
-            std::cout << "Process:" << iterator->process->size << std::endl;
-
+        if (iterator->size > 0)
+            std::cout << "Avaliable Size:" << iterator->size << std::endl;
+        else
+            std::cout << (0 - iterator->size) << std::endl;
         iterator = iterator->next;
     }
 }
 
-void next_fit(int size) {
-    while (_memory != nullptr) {
-        if (_memory->block == nullptr || _memory->block->size <= size) {
-            //Insere um novo processo
-            _memory->process = new (struct process);
-            _memory->process->size = size;
-            _memory->next += sizeof(size);
-            //Reduz o tamanho do processo do Bloco
-
-            return;
+void *first_fit(int size) {
+    auto *iterator = _memory;
+    struct memory *previous = nullptr;
+    int _size = size + sizeof(struct memory);
+    while (iterator != nullptr) {
+        if (iterator->size >= _size) {
+            previous = iterator;
+            iterator = iterator + sizeof(struct memory) + size;
+            iterator->size = previous->size - size - (sizeof(struct memory));
+            iterator->next = previous->next;
+            previous->next = iterator;
+            previous->size = 0 - size;
+            std::cout << previous + sizeof(struct memory) << std::endl;
+        } else {
+            if (iterator->size > size) {
+                iterator->size = iterator->size * -1;
+                std::cout << iterator + sizeof(struct memory) << std::endl;
+            }
         }
-        _memory = _memory->next;
+        iterator = iterator->next;
+    }
+}
+
+struct memory *_next = nullptr;
+
+void *next_fit(int size) {
+    struct memory *iterator;
+    struct memory *previous = nullptr;
+    int _size = size + sizeof(struct memory);
+
+    if (_next->next == nullptr)
+        iterator = _memory;
+    else
+        iterator = _next->next;
+
+    while (iterator != nullptr) {
+        if (iterator->size >= _size) {
+            previous = iterator;
+            iterator = iterator + sizeof(struct memory) + size;
+            iterator->size = previous->size - size - (sizeof(struct memory));
+            iterator->next = previous->next;
+            previous->next = iterator;
+            previous->size = 0 - size;
+            _next = iterator;
+            std::cout << previous + sizeof(struct memory) << std::endl;
+        } else {
+            if (iterator->size > size) {
+                iterator->size = iterator->size * -1;
+                _next = iterator;
+                std::cout << iterator + sizeof(struct memory) << std::endl;
+            }
+        }
+        iterator = iterator->next;
     }
 
-    std::cout << "The process couldn't be inserted" << std::endl;
+    iterator = _memory;
+    while (iterator != _next) {
+        if (iterator->size >= _size) {
+            previous = iterator;
+            iterator = iterator + sizeof(struct memory) + size;
+            iterator->size = previous->size - size - (sizeof(struct memory));
+            iterator->next = previous->next;
+            previous->next = iterator;
+            previous->size = 0 - size;
+            _next = iterator;
+            std::cout << previous + sizeof(struct memory) << std::endl;
+        } else {
+            if (iterator->size > size) {
+                iterator->size = iterator->size * -1;
+                _next = iterator;
+                std::cout << iterator + sizeof(struct memory) << std::endl;
+            }
+        }
+        iterator = iterator->next;
+    }
 }
 
 int main(int argc, char *argv[]) {
     inicializa_mem();
     next_fit(10);
     next_fit(20);
-    next_fit(30);
     mostra_mem();
 
     return 0;
